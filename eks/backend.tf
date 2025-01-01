694759578e12985b92cff290e2cba1a18106cf14
sudharsanpv <sudharsanpv@gmail.com>
2025-01-01 15:24:04 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
-    region         = "us-east-2"
+    bucket         = "my-ews-baket786"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
